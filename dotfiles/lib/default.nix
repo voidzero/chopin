97c735c2b058e194a2738fc121b888dc61e72848
Mark van Dijk <github@voidzero.net>
2022-11-04 17:01:19 +0100

    Removed jdisaacs' config in favor of Misterio77

--- dotfiles/lib/default.nix
+++
@@ -1,7 +0,0 @@
-# vim: ts=2 sw=2 ai et si sta fdm=marker
-# dotfiles/lib/default.nix
-{ pkgs, home-manager, system, lib, overlays, ... }:
-rec {
-  user = import ./user.nix { inherit pkgs home-manager lib system overlays; };
-  host = import ./host.nix { inherit system pkgs home-manager lib user; };
-}
